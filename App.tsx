977926f6a4987b021b08adb9ac71609bc9a26986
jackkarthi <kk7659796@gmail.com>
2024-12-01 18:35:34 +0530

    background color remove

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function App(){
   return(
-    <View style={{alignItems:'center',flex:1,backgroundColor:'green',justifyContent:'center'}}>
+    <View style={{alignItems:'center',flex:1,justifyContent:'center'}}>
       <Text>task diary</Text>
     </View>
   )
